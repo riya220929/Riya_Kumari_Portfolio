8a15ab1e536f5c18d9d27f0e02c827d7daf3cad0
riya220929 <riyakumari220929@gmail.com>
2025-06-11 22:11:36 +0530

    Made navbar responsive with hamburger menu

--- script.js
+++ script.js
@@ -86,3 +86,9 @@ if (contactForm) {
   });
 }
 
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
